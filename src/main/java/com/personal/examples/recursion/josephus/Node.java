8b736c3dd412eaf0606de170adab46cba43cedc4
jatin <jatin@192.168.0.103>
2019-08-17 10:10:16 +0530

    Committing the Josephus problem

---
+++ src/main/java/com/personal/examples/recursion/josephus/Node.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.personal.examples.recursion.josephus;
+
+/**
+ * @author jatin
+ *
+ */
+public class Node {
+
+	private int value;
+	private Node nextNode;
+	private Node previousNode;
+	
+	public Node(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the nextNode
+	 */
+	public Node getNextNode() {
+		return nextNode;
+	}
+
+	/**
+	 * @param nextNode the nextNode to set
+	 */
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	/**
+	 * @return the previousNode
+	 */
+	public Node getPreviousNode() {
+		return previousNode;
+	}
+
+	/**
+	 * @param previousNode the previousNode to set
+	 */
+	public void setPreviousNode(Node previousNode) {
+		this.previousNode = previousNode;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Node [value=" + value + ", nextNode=" + nextNode + ", previousNode=" + previousNode + "]";
+	}
+}
