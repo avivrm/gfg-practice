32e9f4fb61c2248a6539c5be5bbde3f92117172c
jatin <jatin@jatin-PC.lan>
2019-09-16 07:36:59 +0530

    Tweaked the Merge Sort program to implement the inversion count program. Committing the input file with 1 lac entries as well

--- src/main/java/com/gfg/practice/jatin/recursion/mergesort/MergeSort.java
+++ src/main/java/com/gfg/practice/jatin/recursion/mergesort/MergeSort.java
@@ -3,7 +3,11 @@
  */
 package com.gfg.practice.jatin.recursion.mergesort;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jatin
@@ -11,9 +15,22 @@ import java.util.Arrays;
  */
 public class MergeSort {
 
-	public static void main(String[] args) {
-		int[] items = {20,70,56,78,23,42,11,89,65,49,61,3,74,40,4,82};
+	public static void main(String[] args) throws InterruptedException {
+//		int[] items = {20,70,56,78,23,42,11,89,65,49,61,3,74,40,4,82};
+//		int[] items = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+//		int[] items = {5, 1};
+		
+		
+		try {
+		int[] items = Files.lines(Paths.get("InputData_InversionCount.txt"))
+        .map(s -> s.trim())
+        .mapToInt(s -> Integer.parseInt(s))
+        .toArray();
+		
 		mergeSort(items, 0, items.length-1);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 		
 	}
 	
@@ -28,6 +45,7 @@ public class MergeSort {
 		merge(items, startCounter, mid, endCounter);
 	}
 	private static int counter = 1;
+	private static long inversionCount = 0;
 	private static void merge(int[] items, int startCounter, int mid, int endCounter) {
 
 		System.out.println("\nIteration :" + counter++ );
@@ -59,18 +77,20 @@ public class MergeSort {
 			} else {
 				items[i] = rightArray[k];
 				k++;
+				inversionCount += leftArray.length - j;
 			}
 			i++;
 		}
-		System.out.print("Left Array: \t");
-		Arrays.stream(leftArray).forEach(n -> System.out.print(n + "\t"));
-		System.out.println();
-		
-		System.out.print("Right Array: \t");
-		Arrays.stream(rightArray).forEach(n -> System.out.print(n + "\t"));
-		System.out.println();
-		Arrays.stream(items).forEach(n -> System.out.print(n + "\t"));
-		System.out.println();
+//		System.out.print("Left Array: \t");
+//		Arrays.stream(leftArray).forEach(n -> System.out.print(n + "\t"));
+//		System.out.println();
+//		
+//		System.out.print("Right Array: \t");
+//		Arrays.stream(rightArray).forEach(n -> System.out.print(n + "\t"));
+//		System.out.println();
+//		Arrays.stream(items).forEach(n -> System.out.print(n + "\t"));
+//		System.out.println();
+		System.out.println("Total Inversions : " + inversionCount);
 		
 	}
 }
